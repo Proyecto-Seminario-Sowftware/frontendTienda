86a8a4ca12ac875be3ed2a9f45973d0abc6f0033
Luis Dario <luisda13@yahoo.es>
2019-12-16 08:29:42 -0600

    Configuracion para express para heroku

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 // importar express
-var express = require("express");
-var path = require("path");
-var serveStatic = require("serve-static");
+const express = require("express");
+const path = require("path");
 
 // crear al app
 app = express();
 
-// Hacer publica la carpeta  diss
-app.use(serveStatic(__dirname + "/dist"));
+app.use(express.static(_dirname + "/dist"));
+app.get("/.*/", function(req, res) {
+  res.sendfile(__dirname + "/dist/index.html");
+});
 
 // Enviarle el puerto
-var port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port);
 console.log("server started " + port);
